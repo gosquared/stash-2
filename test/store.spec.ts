1732f6fe52534ec959775f9fe3c7ae4c2cc40517
Geoff Wagstaff <geoff@gosquared.com>
2021-08-24 15:39:09 +0000

    add tests

--- test/store.spec.ts
+++ test/store.spec.ts
@@ -1,3 +1,9 @@
+import { spy, stub } from 'sinon';
+import { Stash } from '../src/main';
+import { expect } from 'chai';
+import faker from 'faker';
+import IORedis from 'ioredis';
+
 describe('stashing new value', () => {
   it('should store in lru');
   it('should store in redis');
@@ -12,6 +18,20 @@ describe('getting a value', () => {
   it('should come from lru if stashed');
   it('should come from redis if not in lru');
   it('should fetch if not in redis');
-  it('should save value in redis');
-  it('should save value in lru');
+  it('should save value in lru', async () => {
+    const key = faker.datatype.uuid();
+    const fetch = async () => 'test';
+    const stash = new Stash();
+    await stash.get(key, fetch);
+    expect(stash.lru.has(key)).equals(true);
+  });
+  it('should save value in redis', async () => {
+    const key = faker.datatype.uuid();
+    const fetch = async () => 'test';
+    const stash = new Stash();
+    await stash.get(key, fetch);
+    const redis = new IORedis();
+    const result = await redis.exists(key);
+    expect(result).equals(1);
+  });
 })
